185e7d8e1801b5d3fdf3bcc23bb8853d85707cf0
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-27 23:42:41 -0300

    Resolução do problema 'CarRental' versão NelioAlvesFalando

---
+++ CarRentalNelioResolution/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using CarRentalNelioResolution.Entities;
+using CarRentalNelioResolution.Services;
+
+namespace CarRentalNelioResolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.Parse(Console.ReadLine()); //Alternativa: datetime.parseExact(console.readline(), "dd/MM/yyyy hh:mm");
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.Parse(Console.ReadLine()); //datetime.parseExact(console.readline(), "dd/MM/yyyy hh:mm");
+            
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model)); //Bacana
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
+
+            rentalService.ProcessInvoice(carRental);
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
